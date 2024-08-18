9e59834388af59c3ad7d455112f27d6dee0cf084
Rafael-Guerra-42 <guerra.rag.12@outlook.com>
2024-08-18 11:30:33 -0300

    closes #6 - Cadastro e feito apenas se estiver tudo correto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+indexToIncrement = 0;
 function GetAdress(){
     var CEP = document.getElementById("typedCEP").value;
     $.getJSON(`https://viacep.com.br/ws/${CEP}/json/`,(reply) =>
@@ -37,6 +38,51 @@ function CleanCEPForm(){
     document.getElementById("neighbourhood").value="";
     document.getElementById("city").value="";
     document.getElementById("state").value="";
+    document.getElementById("number").value="";
 
 
+};
+function CleanInitialForm(){
+    document.getElementById("name").value="";
+    document.getElementById("lastName").value="";
+    document.getElementById("typedCEP").value="";
+
+};
+
+function AddForm(){
+    document.getElementById("alertPlace").innerHTML="";
+    if(document.getElementById("name").value=="" || document.getElementById("lastName").value=="" || document.getElementById("typedCEP").value=="" || document.getElementById("number").value=="" ){
+        console.log("erro");
+        
+        document.getElementById("alertPlace").innerHTML+=
+        `
+        <div class="alert alert-warning" id="alert">
+            <strong>Formulário Inconpleto.</strong>
+        </div>
+        `;
+
+
+
+    }
+    else{
+        indexToIncrement++;
+        var fullName = document.getElementById("name").value + " " + document.getElementById("lastName").value;
+        document.getElementById("dataInsertion").innerHTML+=
+        `
+        <div class="row text-center">   
+            <div class="col-1">${indexToIncrement}</div>
+            <div class="col-3">${fullName}</div>
+            <div class="col-2">${document.getElementById("adress").value}${" "}${document.getElementById("number").value}</div>
+            <div class="col-1">${document.getElementById("typedCEP").value}</div>
+            <div class="col-2">${document.getElementById("neighbourhood").value}</div>
+            <div class="col-2">${document.getElementById("city").value}</div>
+            <div class="col-1">${document.getElementById("state").value}</div>
+            <hr>
+        </div>
+        `;
+
+    }
+    CleanCEPForm();
+    CleanInitialForm();
+    document.getElementById("number").disabled=true;
 };
