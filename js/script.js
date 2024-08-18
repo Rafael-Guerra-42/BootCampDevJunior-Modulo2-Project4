1d670ea0a37f19ce7125f4b212b083e4dfe02c33
Rafael-Guerra-42 <guerra.rag.12@outlook.com>
2024-08-18 10:53:27 -0300

    closes #3 - Verificação de CEP

--- js/script.js
+++ js/script.js
@@ -2,18 +2,39 @@ function GetAdress(){
     var CEP = document.getElementById("typedCEP").value;
     $.getJSON(`https://viacep.com.br/ws/${CEP}/json/`,(reply) =>
     {
-        document.getElementById("adress").value = JSON.stringify(reply.logradouro).replace(/['"]+/g, '');
-        document.getElementById("neighbourhood").value = JSON.stringify(reply.bairro).replace(/['"]+/g, '');
-        document.getElementById("city").value = JSON.stringify(reply.localidade).replace(/['"]+/g, '');
-        document.getElementById("state").value = JSON.stringify(reply.uf).replace(/['"]+/g, '');
-        /*
-        if (JSON.stringify(reply.error)=="true"){
-            console.log("erro");
-
-
+        if (document.getElementById("typedCEP").value==""){
+            CleanCEPForm();
+            document.getElementById("alertPlace").innerHTML="";
+            
         }
-        */
+        if (("erro" in reply)){
+            CleanCEPForm();
+            
+            console.log("erro");
+            document.getElementById("alertPlace").innerHTML="";
+            document.getElementById("alertPlace").innerHTML+=
+            `
+            <div class="alert alert-warning" id="alert">
+                <strong>CEP inválido.</strong>
+            </div>
+            `;
 
+        }else{
+
+            document.getElementById("alertPlace").innerHTML="";
+            document.getElementById("adress").value = JSON.stringify(reply.logradouro).replace(/['"]+/g, '');
+            document.getElementById("neighbourhood").value = JSON.stringify(reply.bairro).replace(/['"]+/g, '');
+            document.getElementById("city").value = JSON.stringify(reply.localidade).replace(/['"]+/g, '');
+            document.getElementById("state").value = JSON.stringify(reply.uf).replace(/['"]+/g, '');
+        }
     }
     );
+};
+function CleanCEPForm(){
+    document.getElementById("adress").value="";
+    document.getElementById("neighbourhood").value="";
+    document.getElementById("city").value="";
+    document.getElementById("state").value="";
+
+
 };
